de6ce562454856f90cb7e0cb7df9de137cc318e7
apoorv1807 <45945222+apoorv1807@users.noreply.github.com>
2026-08-30 18:42:01 +0530

    Update graphConnectorMiddleware.js

--- src/middleware/graphConnectorMiddleware.js
+++ src/middleware/graphConnectorMiddleware.js
@@ -3,16 +3,16 @@ import { getConnectorById } from '../helpers/connectorHelper.js';
 const graphConnectorMiddleware = async (req, res, next) => {
     const connectorId = req.header('connector-id');
     if (!connectorId || !ObjectId.isValid(connectorId)) {
-        return res.status(422).json({ message: "Invalid Connector Access" });
+        return res.status(422).json({ message: "Invalid Connector Access1" });
     }
 
     if (!req.header('connector-id')) {
-        return res.status(422).json({ message: "Invalid Connector Access" });
+        return res.status(422).json({ message: "Invalid Connector Access2" });
     }
 
     const connector = await getConnectorById(connectorId);
     if (!connector) {
-        return res.status(422).json({ message: "Invalid Connector Access" });
+        return res.status(422).json({ message: "Invalid Connector Access3" });
     }
     next();
 }
