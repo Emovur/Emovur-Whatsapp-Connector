7a4a3dc25b0531d6a363cb9196950697c34d2fc1
apoorv1807 <45945222+apoorv1807@users.noreply.github.com>
2026-09-19 17:25:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { } from 'dotenv/config';
 momentTz.tz.setDefault("Asia/Calcutta");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 const corsSetup = cors({
     origin: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
